2c740487f2026238cc53b48a14160e578ff77c7d
pnayak1501 <pnayak1501@gmail.com>
2020-09-15 19:48:41 +0530

    numpy3

---
+++ numpy3.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+var1 = np.linspace(10,50,4)
+# print(var1)
+
+var2 = np.array([[2,4,6],[1,3,5]])
+# print(var2[0,1])
+# print(var2[0:,1])
+
+var3 = np.array([2,4,6])
+print(var3.max())
+print(var3.min())
+print(var3.sum())
