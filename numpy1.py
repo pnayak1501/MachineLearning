9b6c70a210faeb872d9e0fe57628dc98866754d5
pnayak1501 <pnayak1501@gmail.com>
2020-09-15 19:07:44 +0530

    numpy1

---
+++ numpy1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+var1 = np.array([2,4,6]) #creating one-D array
+print(var1)
+
+var2 = np.array([[2,4,6],[1,3,5]])
+# print(var2)
+var3 = np.array([[2,4,6],[1,3,5]])
+print(var2+var3)
