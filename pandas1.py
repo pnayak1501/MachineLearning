bb69e5857903bc16933e1345a62946326e9f32c4
pnayak1501 <pnayak1501@gmail.com>
2020-09-16 08:24:16 +0530

    pandas1

---
+++ pandas1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+Employee = {'number':[1,2,3,4,5],'name':['AB','Virat','Finchy','Ali','Maxwell'],'Hourly salary':[15,25,3,523,10]}
+table1 = pd.DataFrame(Employee)
+print(table1)
+print(Employee)
