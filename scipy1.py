a8895d8b4847eefd6ba281332ad7d156d17593e4
pnayak1501 <pnayak1501@gmail.com>
2020-09-17 21:08:30 +0530

    scipy1

---
+++ scipy1.py
@@ -0,0 +1,9 @@
+import scipy as sp
+from scipy import integrate
+from scipy import cluster
+from scipy import fftpack
+
+# help(integrate)
+sp.info(fftpack)            #To get info about any sub-package
+sp.source(cluster)          #To get the source code of any subpackage
+
