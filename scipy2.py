ce4357b2cb351413002b301738ae779575045deb
pnayak1501 <pnayak1501@gmail.com>
2020-09-17 21:08:48 +0530

    scipy2

---
+++ scipy2.py
@@ -0,0 +1,17 @@
+import scipy as sp
+from scipy import special
+
+fun1 = special.kelvin(15)
+print(fun1)
+
+fun2 = special.xlogy(2,10)
+print(fun2)
+
+fun3 = special.exp10(50)
+print(fun3)
+
+fun4 = special.sindg(30)
+print(fun4)
+
+fun5 = special.cosdg(90)
+print(fun5)
