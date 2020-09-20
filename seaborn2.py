dab4d7ac0ebf94314f56950e4040f70d97c6a5d6
pnayak1501 <pnayak1501@gmail.com>
2020-09-20 17:05:14 +0530

    seaborn2

---
+++ seaborn2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+database = sb.load_dataset('tips')
+
+sb.jointplot(x='tip',y='total_bill',data=database)
+plt.show()
